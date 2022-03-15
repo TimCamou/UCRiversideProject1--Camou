a39cddfe056ffcd4b186ae3c88d8ec939cd6dac4
TimCamou <101665165+TimCamou@users.noreply.github.com>
2022-03-15 13:37:58 -0700

    Delete Linux directory

--- Linux/Wk3/dealer_analysis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat 0310_Dealer_schedule 0312_Dealer_schedule 0315_Dealer_schedule | grep $1 | awk '{print $1,$2,$5,$6}' >> Dealer_Working_During_Losses
--- Linux/Wk3/roulette_dealer_finder_by_time_and_game.sh
+++
@@ -1,2 +0,0 @@
-cat $1_Dealer_schedule | awk -F" " '{print $1,$2,"$4,$5"}' | grep "$2" "$3"
-
